058f806b626a9d8a81da72bb27b1bfecda541218
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-09-26 09:55:47 +0200

    feat(home/chart): prepare chart layout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,32 @@
 import "./index.css";
 
 export const HomePage = () => {
-  return <div className="home-page">page</div>;
+  return (
+    <div className="home-content">
+      <div className="home-text">
+        <h2 className="home-title">
+          Bonjour <span className="home-name">Thomas</span>
+        </h2>
+        <p className="home-subtitle">
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </div>
+      <div className="home-charts">
+        <div className="home-activity">
+          <div className="home-barchart">activité quoti</div>
+          <div className="home-subchart">
+            <div>petit graph</div>
+            <div>petit graph</div>
+            <div>petit graph</div>
+          </div>
+        </div>
+        <div className="home-card">
+          <div>data card</div>
+          <div>data card</div>
+          <div>data card</div>
+          <div>data card</div>
+        </div>
+      </div>
+    </div>
+  );
 };
